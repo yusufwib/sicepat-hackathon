cdc32c4d106520f323e90177030a862b87cb226a
Yusuf Wibisono <yusufw2429@gmail.com>
2021-12-15 20:38:51 +0700

    x

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,7 +6,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Favicon -->
-<!-- <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/logo/icon.png')}}"> -->
 
 <!-- CSS
 ============================================ -->
