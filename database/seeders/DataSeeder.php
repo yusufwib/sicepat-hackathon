78efec1040a8d955a753cf896d8f0ab970a4e87d
Yusuf Wibisono <yusufw2429@gmail.com>
2021-12-15 20:34:28 +0700

    x

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -36,13 +36,13 @@ class DataSeeder extends Seeder
             'phone' => '+6281334958665'
         ]);
 
-        for ($i=0; $i < 1; $i++) {
+        for ($i=0; $i < 3; $i++) {
 
             Order::insert([
                 'receiver_phone' => '+6281334958665',
                 'receiver_name' => 'Yusuf Wibisono',
-                'resi' => '00224049041' . $i,
-            'address' => 'Jl. Raya Duri Kosambi No.09, RW.1, Duri Kosambi, Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11750',
+                'resi' => '002240490410',
+                'address' => 'Jl. Raya Duri Kosambi No.09, RW.1, Duri Kosambi, Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11750',
                 'lat' => '-6.164711029198638',
                 'lng' => '106.72373489209119',
                 'status' => 'inactive'
@@ -51,7 +51,7 @@ class DataSeeder extends Seeder
             Order::insert([
                'receiver_phone' => '+6285790905725',
                 'receiver_name' => 'Agus Susanto',
-                'resi' => '00224049041' . $i . 1,
+                'resi' => '002240490411',
                 'address' => '8, RW.7, Rawa Buaya, Cengkareng, West Jakarta City, Jakarta',
                 'lat' => '-6.16360932882379',
                 'lng' => '106.74284460197445',
@@ -61,7 +61,7 @@ class DataSeeder extends Seeder
             Order::insert([
                 'receiver_phone' => '+6281334958665X',
                 'receiver_name' => 'Kenzi Nugroho',
-                'resi' => '00224049041'  . $i . 2,
+                'resi' => '002240490412',
                 'address' => 'Jalan Utan Jati No.109 RT.09, kel, RT.3/RW.11, Pegadungan, Kec. Kalideres, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11840',
                 'lat' => '-6.146700719981047',
                 'lng' => '106.71357295564368',
@@ -71,7 +71,7 @@ class DataSeeder extends Seeder
             Order::insert([
                 'receiver_phone' => '+6281334958665X',
                 'receiver_name' => 'Kevin Lee',
-                'resi' => '00224049041' . $i . 3,
+                'resi' => '002240490413',
                 'address' => 'Jl. Ruko Mutiara Palem Raya No.6, RW.14, Cengkareng Tim., Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11730',
                 'lat' => '-6.137036400151968',
                 'lng' => '106.73555366600293',
@@ -81,7 +81,7 @@ class DataSeeder extends Seeder
             Order::insert([
                 'receiver_phone' => '+6281334958665X',
                 'receiver_name' => 'Dodit Malang',
-                'resi' => '00224049041' . $i . 4,
+                'resi' => '002240490414',
                 'address' => 'Jalan Jelambar, Jl. Kemanggisan, Jl. Jelambar Utama III No.16C, RT.4/RW.8, Jelambar, Kec. Grogol petamburan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11460',
                 'lat' => '-6.151422994248313',
                 'lng' => '106.78205557083328',
@@ -91,7 +91,7 @@ class DataSeeder extends Seeder
             Order::insert([
                 'receiver_phone' => '+6281334958665X',
                 'receiver_name' => 'Mila Klaten',
-                'resi' => '00224049041' . $i . 5,
+                'resi' => '002240490415',
                 'address' => 'AKR Tower, Jl. Perjuangan No.5, RT.11/RW.10, Kb. Jeruk, Kec. Kb. Jeruk, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530',
                 'lat' => '-6.188730745045583',
                 'lng' => '106.76804084102365',
@@ -101,20 +101,20 @@ class DataSeeder extends Seeder
             Order::insert([
                 'receiver_phone' => '+6281334958665X',
                 'receiver_name' => 'Agus PDAM',
-                'resi' => '00224049041' . $i . 6,
+                'resi' => '002240490416',
                 'address' => 'Jl Green Gdn Residence No.8, RT.3/RW.4, Kembangan Utara, Kec. Kembangan, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11520',
                 'lat' => '-6.162579224134279',
                 'lng' => '106.75592638045728',
                 'status' => 'inactive'
             ]);
 
-            Order::insert([
-                'receiver_phone' => '+6281334958665X',
-                'receiver_name' => 'Jiohan Sebastian',
-                 'resi' => '00224049041' . $i . 7,
-               'address' => 'Jl. Hanoman II No. 20, Bojong Indah, Cengkareng, RT.2/RW.9, Rawa Buaya, Cengkareng, RT.2/RW.9, Rawa Buaya, Cengkareng, Jakarta, 11740',
-                'status' => 'inactive'
-            ]);
+            // Order::insert([
+            //     'receiver_phone' => '+6281334958665X',
+            //     'receiver_name' => 'Jiohan Sebastian',
+            //      'resi' => '002240490417',
+            //    'address' => 'Jl. Hanoman II No. 20, Bojong Indah, Cengkareng, RT.2/RW.9, Rawa Buaya, Cengkareng, RT.2/RW.9, Rawa Buaya, Cengkareng, Jakarta, 11740',
+            //     'status' => 'inactive'
+            // ]);
 
 
     //         Order::insert([
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,12 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>GS - {{ $title }}</title>
+<title>SICEPAT - {{ $title }}</title>
 <meta name="robots" content="noindex, follow" />
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Favicon -->
-<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/logo/icon.png')}}">
+<!-- <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/logo/icon.png')}}"> -->
 
 <!-- CSS
 ============================================ -->
