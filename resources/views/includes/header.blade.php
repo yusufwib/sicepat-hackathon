48cf8a7347f1372663f6c153b9040817347b617a
Yusuf Wibisono <yusufw2429@gmail.com>
2021-12-15 20:42:39 +0700

    x

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,6 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/logo/sicepat.png')}}">
 
 <!-- CSS
 ============================================ -->
